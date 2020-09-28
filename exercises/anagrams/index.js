72b56139ced89fb27516f7f7ed96ff91f3b364e7
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-28 12:24:00 -0700

    completed Anagrams problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 
 module.exports = anagrams;
+
+// Solution #1
+// function anagrams(stringA, stringB) {
+//     a1 = stringA.replace(/[^\w]/g, "").toLowerCase();
+//     a2 = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+//     if (a1.length !== a2.length) {
+//       return false;
+//     }
+
+//     let freqCount1 = {};
+//     let freqCount2 = {};
+
+//     for (let val of a1) {
+//       freqCount1[val] = (freqCount1[val] || 0) + 1;
+//     }
+//     for (let val of a2) {
+//       freqCount2[val] = (freqCount2[val] || 0) + 1;
+//     }
+
+//     for (let key in freqCount1) {
+//       if (!(key in freqCount2)) {
+//         return false;
+//       }
+//       if (freqCount2[key] !== freqCount1[key]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
