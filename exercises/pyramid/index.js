6c555bf9ff2dcd70ea9c6bce294f48ec734a055a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-29 10:27:50 -0700

    completed pyrdamid problem with Recursion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const mid = Math.floor((2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let steps = "";
+const steps = require("../steps");
 
-    for (let col = 0; col < 2 * n - 1; col++) {
-      if (mid - row <= col && mid + row >= col) {
-        steps += "#";
-      } else {
-        steps += " ";
-      }
-    }
-    console.log(steps);
+function pyramid(n, row = 0, steps = "") {
+  if (row === n) {
+    return;
   }
+  if (steps.length === 2 * n - 1) {
+    console.log(steps);
+    return pyramid(n, row + 1);
+  }
+  const mid = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (mid - row <= steps.length && mid + row >= steps.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, steps + add);
 }
 
 module.exports = pyramid;
+
+// Solution #1
+
+// function pyramid(n) {
+//     const mid = Math.floor((2 * n - 1) / 2);
+//     for (let row = 0; row < n; row++) {
+//       let steps = "";
+
+//       for (let col = 0; col < 2 * n - 1; col++) {
+//         if (mid - row <= col && mid + row >= col) {
+//           steps += "#";
+//         } else {
+//           steps += " ";
+//         }
+//       }
+//       console.log(steps);
+//     }
+//   }
