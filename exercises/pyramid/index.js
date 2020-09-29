351517518d2955d9758d03cbfcfa4e01be06f17a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-29 10:17:00 -0700

    completed pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let steps = "";
+
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (mid - row <= col && mid + row >= col) {
+        steps += "#";
+      } else {
+        steps += " ";
+      }
+    }
+    console.log(steps);
+  }
+}
 
 module.exports = pyramid;
